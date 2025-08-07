c31afdabf81cff56f399da57e7308544b2d73d76
ItsOm-Dev <itsom2802@gmail.com>
2025-08-07 10:10:01 +0530

    Update config.py

--- modi2marathi/config.py
+++ modi2marathi/config.py
@@ -61,14 +61,14 @@ DRIVE_BASE = "/content/drive/MyDrive/modi2marathi"
 # IDX_TO_CHAR_PATH = os.path.join(BASE_DIR, "modi_dataset_syn/idx_to_char.json")
 
 ###################   Mixed Dataset  ################################
-IMAGE_DIR = os.path.join(BASE_DIR, "mixed/images")
-LABEL_DIR = os.path.join(BASE_DIR, "mixed/labels")
+# IMAGE_DIR = os.path.join(BASE_DIR, "mixed/images")
+# LABEL_DIR = os.path.join(BASE_DIR, "mixed/labels")
 CHAR_TO_IDX_PATH = os.path.join(BASE_DIR, "mixed/char_to_idx.json")
 IDX_TO_CHAR_PATH = os.path.join(BASE_DIR, "mixed/idx_to_char.json")
 
 ###################   ORG Dataset  ################################
-# IMAGE_DIR = os.path.join(BASE_DIR, "modi_dataset/images")
-# LABEL_DIR = os.path.join(BASE_DIR, "modi_dataset/labels")
+IMAGE_DIR = os.path.join(BASE_DIR, "modi_dataset/images")
+LABEL_DIR = os.path.join(BASE_DIR, "modi_dataset/labels")
 # CHAR_TO_IDX_PATH = os.path.join(BASE_DIR, "modi_dataset/char_to_idx.json")
 # IDX_TO_CHAR_PATH = os.path.join(BASE_DIR, "modi_dataset/idx_to_char.json")
 
@@ -83,7 +83,7 @@ def get_checkpoint_path(epoch):
 
 # ========== ✅ Training Hyperparameters ==========
 BATCH_SIZE = 4
-EPOCHS = 65  # 45 WILL BE SEMI FINAL
+EPOCHS = 80  # 45 WILL BE SEMI FINAL
 LEARNING_RATE = 1e-4
 IMG_HEIGHT = 128
 IMG_WIDTH = 720
@@ -214,6 +214,7 @@ if __name__ == "__main__":
 #     print("IMAGE_DIR:", IMAGE_DIR)
 #     print("LABEL_DIR:", LABEL_DIR)
 #     print("CHECKPOINT_DIR:", CHECKPOINT_DIR)
+
 
 
 
